34a1f354d7b65828e4fa309f7591fbe2e2820d0d
afshanahmeda2k <afshanahmeda2k@gmail.com>
2021-08-25 15:38:38 +0530

    changed window color and increased window border in config.h

--- config.h
+++ config.h
@@ -12,11 +12,13 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_win_border[]  = "#263238";
+static const char col_win_border_sel[]  = "#ffff00";
+static const char col_win_border_norm[] = "#bf360c";
+static const char col_win_bg_norm[] = "#222222";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_win_border  },
+	[SchemeNorm] = { col_gray3, col_win_bg_norm, col_win_border_norm },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_win_border_sel  },
 };
 
 /* tagging */
