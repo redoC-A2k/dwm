d19d200fd44fd9389a8164debb400c253c7fb807
afshanahmeda2k <afshanahmeda2k@gmail.com>
2021-08-26 09:13:32 +0530

    added pywal related changes in colors to dwm

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono NF:size=11","Font Awesome:size 15" };
-static const char dmenufont[]       = "JetBrainsMono NF:size=10";
+static const char dmenufont[]       = "JetBrainsMono NF:size=11";
 
 // static const char col_gray1[]       = "#222222";
 // static const char col_gray2[]       = "#444444";
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenucmd[] = { "dmenu_run","-fn",dmenufont, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
