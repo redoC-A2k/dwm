88974e8216b7e63b486a688242f9cf00c1863ec6
afshanahmeda2k <afshanahmeda2k@gmail.com>
2021-08-26 16:33:07 +0530

    added useless gap patch

--- config.def.h
+++ config.def.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono NF:size=11","Font Awesome:size 15" };
-static const char dmenufont[]       = "JetBrainsMono NF:size=10";
+static const char *fonts[]          = { "JetBrainsMono NF:size=11","JoyPixels:pixelsize=11:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrainsMono NF:size=11";
 
 // static const char col_gray1[]       = "#222222";
 // static const char col_gray2[]       = "#444444";
@@ -22,8 +22,21 @@ static const char dmenufont[]       = "JetBrainsMono NF:size=10";
 //  [SchemeSel]  = { col_gray4, col_cyan,  col_win_border_sel  },
 // };
 
+ static char normbgcolor[]           ="#222222";
+ static char normbordercolor[]       ="#444444";
+ static char normfgcolor[]           = "#bbbbbb";
+ static char selfgcolor[]            = "#eeeeee";
+ static char selbordercolor[]        = "#005577";
+ static char selbgcolor[]            = "#005577";
+ static char *colors[][3] = {
+        /*               fg           bg           border   */
+        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+ };
+
+
 // pywal color scheme importing
-#include "/home/afshan/.cache/wal/colors-wal-dwm.h"
+//#include "/home/afshan/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -63,7 +76,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
