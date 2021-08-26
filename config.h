c7fd11b87d46f1ace85c6da256a456e66d515ae9
afshanahmeda2k <afshanahmeda2k@gmail.com>
2021-08-26 21:22:50 +0530

    added joypixels, noto-fonts-emoji ,twemoji

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gap pixel between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 3;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono NF:size=11","JoyPixels:pixelsize=11:antialias=true:autohint=true" };
+static const char *fonts[]          = { "JetBrainsMono NF:size=11","joypixels:pixelsize=13:antialias=true:autohint=true","noto-fonts-emoji:pixelsize=13:antialias=true:autohint=true","twemoji:pixelsize=13:antialias=true:autohint=true" };
 static const char dmenufont[]       = "JetBrainsMono NF:size=11";
 
 // static const char col_gray1[]       = "#222222";
